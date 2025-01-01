8b971da5451e35212ae0d0c1462b749626fff65d
Niels <niels.anbeek@live.nl>
2025-01-01 15:59:03 +0100

    created first forms

--- 2 - projects/flask-backend/src/website/auth.py
+++ 2 - projects/flask-backend/src/website/auth.py
@@ -1,15 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return 'Login'
+    return render_template('login.html')
 
 @auth.route('/logout')
 def logout():
     return 'Logout'
 
-@auth.route('/signup')
+@auth.route('/signup', methods=["GET", "POST"])
 def signup():
-    return 'Signup'
+    if request.method == "POST":
+        #Input validation
+
+        return render_template('signup.html')
+
+
+    
+    return render_template('signup.html')
